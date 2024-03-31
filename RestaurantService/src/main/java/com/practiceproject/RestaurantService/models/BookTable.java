7a8af3cdcdac97f8cdcaff1fa30d7ca8d5cb2e4e
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-03-31 18:24:24 +0530

    Working with strings

---
+++ RestaurantService/src/main/java/com/practiceproject/RestaurantService/models/BookTable.java
@@ -0,0 +1,15 @@
+package com.practiceproject.RestaurantService.models;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class BookTable {
+    public String bookingId;
+    public Integer totalBill;
+    public
+}
