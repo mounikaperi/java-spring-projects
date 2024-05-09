9f36a04614befe5fcf35aa20aa36c15d284fd5ba
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-05-09 14:07:24 +0530

    Working with DateTimeAPI's - Part2

--- OCPCoreJava17/src/com/ocp17/datetimeapi/DateTimeAPI2.java
+++ OCPCoreJava17/src/com/ocp17/datetimeapi/DateTimeAPI2.java
@@ -65,16 +65,69 @@ public class DateTimeAPI2 {
 		
 		System.out.println("-------------------------------------------");
 		var zone = ZoneId.of("US/Eastern");
-		var zone1 = ZonedDateTime.of(date1, time1, zone);
+		var zone1 = ZonedDateTime.of(2022, 1, 20, 6, 15, 30, 200, zone);
+		var zone2 = ZonedDateTime.of(date1, time1, zone);
 		var zone3 = ZonedDateTime.of(dateTime1, zone);
 		
 		System.out.println("zone US/Eastern: " + zone);
-		System.out.println("ZonedDateTime: " + zone1);
-		System.out.println("ZonedDateTime with datetime: " + zone3);
+		System.out.println("ZonedDateTime (int year, int month, int dayOfMonth, int hour, int second, int nanos, ZoneId zone): " + zone1);
+		System.out.println("ZonedDateTime (LocalDate date, LocalTime time, ZoneId zone): " + zone2);
+		System.out.println("ZonedDateTime (LocalDateTime dateTime, ZoneId zone): " + zone3);
 		
 		System.out.println("-------------------------------------------");
 		
+		var date = LocalDate.of(2022, Month.JANUARY, 20);
+		System.out.println("LocalDate (int year, Month month, int dayOfMonth: "+ date);
+		date = date.plusDays(2);
+		System.out.println("plusDays: " + date);
+		date = date.plusWeeks(1);
+		System.out.println("plusWeeks: " + date);
+		date = date.plusMonths(1);
+		System.out.println("plusMonths: " + date);
+		date = date.plusYears(5);
+		System.out.println("plusYears: " + date);
+		
+		System.out.println("-------------------------------------------");
+		
+		var date4 = LocalDate.of(2024, Month.JANUARY, 20);
+		var time4 = LocalTime.of(5,  15);
+		var dateTime7 = LocalDateTime.of(date4, time4);
+		System.out.println("LocalDateTime: " + dateTime7);
+		dateTime7 = dateTime7.minusDays(1);
+		System.out.println("minusDays: " + dateTime7);
+		dateTime7 = dateTime7.minusHours(10);
+		System.out.println("minusHours: " + dateTime7);
+		dateTime7 = dateTime7.minusSeconds(30);
+		System.out.println("minusSeconds: " + dateTime7);
+		
+		System.out.println("-------------------------------------------");
+		var date5 = LocalDate.of(2024, Month.JANUARY, 20);
+		var time5 = LocalTime.of(5, 15);
+		var dateTime8 = LocalDateTime.of(date5, time5).minusDays(1).minusHours(10).minusSeconds(30);
+		System.out.println("Chained minusDays, minusHours, minusSeconds: " + dateTime8);
+		
+		System.out.println("-------------------------------------------");
+		var start = LocalDate.of(2022, Month.JANUARY, 1);
+		var end = LocalDate.of(2022, Month.MARCH, 30);
+		performAnimalEnrichment(start, end);
+		
+		System.out.println("-------------------------------------------");
+		var period = Period.ofMonths(1); // Create a period
+		performAnimalEnrichment(start, end, period);
 		
 	}
-
+	static void performAnimalEnrichment(LocalDate start, LocalDate end) {
+		var upTo = start;
+		while (upTo.isBefore(end)) {
+			upTo = upTo.plusMonths(1);
+		}
+		System.out.println("adding months using plusMonths: " + upTo);
+	}
+	static void performAnimalEnrichment(LocalDate start, LocalDate end, Period period) {
+		var upTo = start;
+		while (upTo.isBefore(end)) {
+			upTo = upTo.plus(period);
+		}
+		System.out.println("Adding months by creating Period: "+ upTo);
+	}
 }
