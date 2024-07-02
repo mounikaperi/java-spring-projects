1bcb5969b88f60d05f143e61f004957a5fb7ca7f
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-07-02 21:00:30 +0530

    Update GraphsRepresentation.java

--- DSAWithJava/src/com/java/dsa/graphs/GraphsRepresentation.java
+++ DSAWithJava/src/com/java/dsa/graphs/GraphsRepresentation.java
@@ -7,7 +7,7 @@ class GraphRepresentation {
         int n=3, m=3;
         ArrayList<ArrayList<Integer> adj = new ArrayList<ArrayList<Integer>>();
         for (int i=0; i<=n; i++) {
-            adj.add(new ArrayList<Integer>);
+            adj.add(new ArrayList<Integer>());
         }
         adj.get(1).add(2);
         adj.get(2).add(1);
