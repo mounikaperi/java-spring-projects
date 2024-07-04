c71a82eb5a7055ccf983c4ce06b231e8d214eab3
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-07-04 11:30:35 +0530

    Update MergeNodesInBetweenZeros.java

--- DSAWithJava/src/com/java/dsa/linkedlists/MergeNodesInBetweenZeros.java
+++ DSAWithJava/src/com/java/dsa/linkedlists/MergeNodesInBetweenZeros.java
@@ -1,1 +1,20 @@
-
+class MergeNodesInBetweenZeros {
+    public ListNode mergeNodes(ListNode head) {
+        int sum = 0;
+        ListNode curr = head, temp = head.next, lastCheck = null;
+        while (curr.next != null) {
+            sum = 0;
+            while (temp.val != 0) {
+                sum += temp.val;
+                temp = temp.next;
+            }
+            curr.val = sum;
+            curr.next = temp;
+            lastCheck = curr;
+            curr = curr.next;
+            temp = curr.next;
+        }
+        lastCheck.next = null;
+        return head;
+    }  
+}
