da27f8414b10a5fc9ca2cffa7b635dbdf071d41f
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-10-08 18:13:06 +0530

    PathsInJava7

---
+++ OCPCoreJava17/src/com/ocp17/java7/PathsInJava7.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.net.URI;
+
+public class PathsInJava7 {
+    private static String HOME_DIR = "C:\\";
+    public static void main(String[] args) throws IOException {
+      buildPath();
+    }
+    private static void buildPath() throws IOException {
+      Path path = Paths.get(HOME_DIR, "java7", "Test.txt");
+      Path fileName = path.getFileName();
+      System.out.println("The file name is: " + fileName.toString());
+      System.out.println("The File system is: " + path.getFileSystem().toString());
+      System.out.println("The File system separator is: " + path.getFileSystem().getSeparator());
+      for (int i=0; i<path.getNameCount(); i++) {
+        Path subPath = path.getName(i);
+        System.out.println("Path location at index: " + (i+1) + " is " + subPath.toString());
+      }
+      System.out.println("Sub path is: " + path.subpath(0, 2));
+      System.out.println("Root path is: " + path.getRoot());
+      System.out.println("Parent path is: " + path.getParent());
+      Path redundantPath = Paths.get(HOME_DIR, "java7", ".", "Test.txt");
+      System.out.println("Redundant Path is: " + redundantPath.toString());
+      Path cleanPath = redundantPath.normalize();
+      System.out.println("Clean path is: " + cleanPath.toString());
+      URI uri = cleanPath.toUri();
+      System.out.println("URI path is: " + uri.toString());
+      System.out.println("Absolute path is: " + cleanPath.toAbsolutePath().toString());
+      Path partialPath = Paths.get(HOME_DIR, "java7");
+      Path combinedPath = partialPath.resolve("Test.txt");
+      System.out.println("Combined path is " + combinedPath.toString());
+      if (!partialPath.equals(combinedPath)) {
+        System.out.println("Paths are not equal");
+      }
+    }
+}
