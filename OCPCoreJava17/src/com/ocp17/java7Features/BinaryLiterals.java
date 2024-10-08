46f540632a2e06a51fe5da86ec5208874a2f6ec9
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-10-08 11:35:11 +0530

    BinaryLiterals

---
+++ OCPCoreJava17/src/com/ocp17/java7Features/BinaryLiterals.java
@@ -0,0 +1,17 @@
+package com.ocp17.java7Features;
+
+public class BinaryLiterals {
+    public static void main(String[] args) {
+      byte byteBinary = 0b111;
+      short shortBinary = 0B001;
+      int intBinary = 0B101;
+      long longBinary = 0b0000011101100011;
+      System.out.println("The value of byte number is: " + byteBinary);
+      System.out.println("The value of short number is: " + shortBinary);
+      System.out.println("The value of int number is: " + intBinary);
+      System.out.println("The value of long number is: " + longBinary);
+      int number = 5;
+      System.out.println("Is Decimal and Binary value is same? " + (intBinary == number));
+      System.out.println("The value of long after multiplication is: " + (longBinary * 2));
+    }
+}
