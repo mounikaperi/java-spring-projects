17883aaa1f7ee7f10e68bc8b35ac857aac433d92
Sai Mounika Peri <saimounikaperi@gmail.com>
2024-10-08 17:55:37 +0530

    JdbcEnhancements

---
+++ OCPCoreJava17/src/com/ocp17/java7/JdbcEnhancements.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.sql.rowset.JdbcRowSet;
+import javax.sql.rowset.RowSetProvider;
+
+public class JdbcEnhancements {
+    public static void main(String[] args) throws Exception {
+      viewResults();
+      createJdbcRowset();
+    }
+    private static void viewResults() throws Exception {
+      try(Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/organization", "root", "root");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from employee");
+      ) {
+        while (rs.next()) {
+          System.out.println(rs.getString("NAME") + " " + rs.getInt("AGE") + " " + rs.getString("DEPARTMENT"));
+        }
+      } catch (Exception ex) {
+        throw ex;
+      }
+    }
+    private static void createJdbcRowset() throws Exception {
+      try (JdbcRowSet jrs = RowSetProvider.newFactory().createJdbcRowSet();) {
+        jrs.setUrl("jdbc:mysql//localhost:3306/organization");
+        jrs.setUsername("root");
+        jrs.setPassword("root");
+        jrs.setCommand("select * from employee");
+        jrs.execute();
+        while (jrs.next()) {
+          System.out.println(jrs.getString("NAME") + "" + jrs.getInt("AGE") + "" + jrs.getString("DEPARTMENT"));
+        }
+      } catch (Exception ex) {
+        throw ex;
+      }
+    }
+}
